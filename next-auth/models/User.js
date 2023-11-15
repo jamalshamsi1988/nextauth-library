678e78eedd94b231e0e5126e1a619644dfa0f6b0
jamal.shamsi <jamal.shamsi1988@gmail.com>
2023-11-15 23:58:32 +0330

    npm i bcryptjs & mongoose & create connectDB & user

---
+++ next-auth/models/User.js
@@ -0,0 +1,27 @@
+import { Schema,model,models } from "mongoose";
+
+const userSchema = new Schema({
+    email:{
+        type : String,
+        required:true,
+    },
+    password :{
+        type : String,
+        required:true,
+    },
+    name :{
+        type :String,
+    },
+    lastName:{
+        type :String,
+    },
+    createAt :{
+        type :Date,
+        default :()=> Date.now(),
+        immutable :true,
+    },
+});
+
+const User = User.models || model("user" , userSchema);
+
+export default User;
