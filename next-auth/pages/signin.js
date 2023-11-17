d70a1172af4946a8aa5f402cfa5b429203cf760b
jamal.shamsi <jamal.shamsi1988@gmail.com>
2023-11-17 23:19:29 +0330

    use function signIn from next-auth/react in login form

--- next-auth/pages/signin.js
+++ next-auth/pages/signin.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { signIn } from "next-auth/react";
 
-const signIn = () => {
+const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+  const loginHandler = async () => {
+    const res = await signIn("credentials", {
+      email,
+      password,
+      redirect: false,
+    });
+    if(!res.error) router.replace("/dashbord")
+  };
   return (
     <div>
-      
+      <h3>Login Form</h3>
+      <input
+        type="text"
+        value={email}
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        value={password}
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={loginHandler}>Login</button>
     </div>
-  )
-}
+  );
+};
 
-export default signIn
+export default SignIn;
