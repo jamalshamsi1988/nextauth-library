70debbbf6216fee7cc12e1562a88dfc61570bd70
jamal.shamsi <jamal.shamsi1988@gmail.com>
2023-11-18 00:57:46 +0330

    ui for updating info is completed

---
+++ next-auth/pages/api/update-info.js
@@ -0,0 +1,47 @@
+import { getSession } from "next-auth/react";
+import connectDB from "../../utils/connectDB";
+import User from "../../models/User";
+import { verifyPassword } from "../../utils/auth";
+
+async function handler(req, res) {
+  if (req.method !== "POST") return;
+
+  try {
+    await connectDB();
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ status: "failed", message: "Error in connecting DB" });
+  }
+
+  const { name, lastName, password } = req.body;
+  const session = await getSession({ req });
+  if (!session) {
+    return res
+      .status(401)
+      .json({ status: "failed", message: "You are not logged in" });
+  }
+  const user = await User.findOne({ email: session.user.email });
+  if (!user) {
+    return res
+      .status(404)
+      .json({ status: "failed", message: "User dosen't exist" });
+  }
+
+  const isValid = await verifyPassword(password, user.password);
+  if (!isValid) {
+    return res
+      .status(422)
+      .json({ status: "failed", message: "Password is incorrect" });
+  }
+
+  user.name = name;
+  user.lastName = lastName;
+  user.save();
+  res.status(200).json({
+    status: "success",
+    data: { name, lastName, email: session.user.email },
+  });
+}
+export default handler;
--- next-auth/pages/dashbord.js
+++ next-auth/pages/dashbord.js
@@ -1,7 +1,28 @@
 import { getSession } from "next-auth/react";
+import { useEffect, useState } from "react";
 
-const Dashbord = () => {
-  return <div>Dashbord</div>;
+const Dashbord = ({session}) => {
+const [name,setName]=useState("")
+const [lastName , setLastName]=useState("")
+const [password,setPassword]=useState("")
+
+const submitHandler=async()=>{
+  const res = await fetch("/api/update-info" , {
+    method:"POST",
+    body:JSON.stringify({name, lastName , password}),
+    headers :{"Content-Type": "application/json"}
+  });
+  const data=await res.json();
+  console.log(data)
+}
+  return <div>
+    <h1>your email is :{session.user.email}</h1>
+    <h3>Complete your profile info</h3>
+    <input type="text" placeholder="Name" value={name} onChange={e=> setName(e.target.value)} />
+    <input type="text" placeholder="lastName" value={lastName} onChange={e=> setLastName(e.target.value)} />
+    <input type="password" placeholder="Password" value={password} onChange={e=> setPassword(e.target.value)} />
+    <button onClick={submitHandler}>Submit</button>
+  </div>;
 };
 
 export default Dashbord;
