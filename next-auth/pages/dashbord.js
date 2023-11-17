790b6cf7a3876468f8117fef6a4f9c647037f65c
jamal.shamsi <jamal.shamsi1988@gmail.com>
2023-11-17 23:45:59 +0330

    server side authenticated user with getSession

--- next-auth/pages/dashbord.js
+++ next-auth/pages/dashbord.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import { getSession } from "next-auth/react";
 
 const Dashbord = () => {
-  return (
-    <div>
-      Dashbord
-    </div>
-  )
-}
+  return <div>Dashbord</div>;
+};
 
-export default Dashbord
+export default Dashbord;
+
+export async function getServerSideProps({ req }) {
+  const session = await getSession({ req });
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/signin",
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: { session },
+  };
+}
